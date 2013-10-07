0f2675f8ee3a5c409d1078eec6385c5b9bbda33f
Miguel <miguellazev@gmail.com>
2013-10-07 07:33:34 +0100

    First working version.

---
+++ src/GInotify.h
@@ -0,0 +1,95 @@
+/*
+ * GInotify - backend for really simple inotify example
+ *
+ * @Author: Miguel Azevedo <migullazev@gmail.com>
+ */
+
+/* inclusion guard */
+#ifndef __G_INOTIFY_H__
+#define __G_INOTIFY_H__
+
+#include <glib-object.h>
+
+/*
+ * Type macros.
+ */
+#define G_TYPE_INOTIFY                  (g_inotify_get_type ())
+#define G_INOTIFY(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), G_TYPE_INOTIFY, GInotify))
+#define G_IS_INOTIFY(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), G_TYPE_INOTIFY))
+#define G_INOTIFY_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), G_TYPE_INOTIFY, GInotifyClass))
+#define G_IS_INOTIFY_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), G_TYPE_INOTIFY))
+#define G_INOTIFY_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), G_TYPE_INOTIFY, GInotifyClass))
+
+typedef struct _GInotify        GInotify;
+typedef struct _GInotifyClass   GInotifyClass;
+typedef struct _GInotifyPrivate GInotifyPrivate;
+
+struct _GInotify
+{
+  GObject parent_instance;
+
+  /*< private >*/
+  GInotifyPrivate *priv;
+};
+
+struct _GInotifyClass
+{
+  GObjectClass parent_class;
+
+  /* class members */
+};
+
+/* used by G_TYPE_INOTIFY */
+GType g_inotify_get_type (void);
+
+/* Option and event mask macros */
+
+typedef struct GInotifyEvent
+{
+  guint wd;
+  guint32 type;
+  gboolean isdir;
+  guint32 cookie;
+  GString *name;
+} GInotifyEvent;
+
+
+
+#define G_IN_ACCESS		0x00000001	/* File was accessed */
+#define G_IN_MODIFY		0x00000002	/* File was modified */
+#define G_IN_ATTRIB		0x00000004	/* Metadata changed */
+#define G_IN_CLOSE_WRITE	0x00000008	/* Writtable file was closed */
+#define G_IN_CLOSE_NOWRITE	0x00000010	/* Unwrittable file closed */
+#define G_IN_OPEN		0x00000020	/* File was opened */
+#define G_IN_MOVED_FROM		0x00000040	/* File was moved from X */
+#define G_IN_MOVED_TO		0x00000080	/* File was moved to Y */
+#define G_IN_CREATE		0x00000100	/* Subfile was created */
+#define G_IN_DELETE		0x00000200	/* Subfile was deleted */
+#define G_IN_DELETE_SELF	0x00000400	/* Self was deleted */
+#define G_IN_MOVE_SELF          0x00000800      /* Self was moved */
+
+/* the following are legal events.  they are sent as needed to any watch */
+#define G_IN_UNMOUNT		0x00002000	/* Backing fs was unmounted */
+#define G_IN_Q_OVERFLOW		0x00004000	/* Event queued overflowed */
+#define G_IN_IGNORED		0x00008000	/* File was ignored */
+
+/* helper events */
+#define G_IN_CLOSE		(G_IN_CLOSE_WRITE | G_IN_CLOSE_NOWRITE) /* close */
+#define G_IN_MOVE      		(G_IN_MOVED_FROM | G_IN_MOVED_TO) /* moves */
+
+/* special flags */
+#define G_IN_ISDIR		0x40000000	/* event occurred against dir */
+#define G_IN_ONESHOT		0x80000000	/* only send event once */
+
+#define G_IN_ALL_EVENTS	(G_IN_ACCESS | G_IN_MODIFY | G_IN_ATTRIB | G_IN_CLOSE_WRITE | \
+			 G_IN_CLOSE_NOWRITE | G_IN_OPEN | G_IN_MOVED_FROM | G_IN_MOVE_SELF | \
+			 G_IN_MOVED_TO | G_IN_DELETE | G_IN_CREATE | G_IN_DELETE_SELF)
+
+/*
+ * Method definitions.
+ */
+GInotify *g_inotify_new ();
+gint g_inotify_add_watch (GInotify *g_inf, const gchar *name, guint32 mask);
+gint g_inotify_rm_watch (GInotify *g_inf, gint wd);
+
+#endif /* __G_INOTIFY_H__ */
---
+++ src/g-inotify-test.c
@@ -0,0 +1,214 @@
+/*
+ * ginotify-test.c - test frontend GInotify
+ *
+ * @Author: Miguel Azevedo <miguellazev@gmail.com>
+ */
+
+
+#include "GInotify.h"
+
+void dir_callback (GInotify *inf, gpointer data)
+{
+  GInotifyEvent *ev = (GInotifyEvent*) data;
+  GString* name;
+
+  if (ev->name == NULL)
+    {
+      const gchar* name_chr = "The watch";
+      name = g_string_new (name_chr);
+    }
+  else
+    name = ev->name;
+
+  const char* type = (ev->isdir == TRUE) ? "dir" : "file";
+
+  switch (ev->type)
+    {
+    case G_IN_MOVED_FROM:
+      g_print ("wd=%d: %s (%s) was moved away,", ev->wd, name->str, type);
+      break;
+
+    case G_IN_MOVED_TO:
+      g_print ("wd=%d: %s (%s) was moved here,", ev->wd, name->str, type);
+      break;
+
+    case G_IN_DELETE:
+      g_print ("wd=%d: %s (%s) was deleted,", ev->wd, name->str, type);
+      break;
+
+    case G_IN_CREATE:
+      g_print ("wd=%d: %s (%s) was created,", ev->wd, name->str, type);
+      break;
+
+    default:
+      g_print ("This means trouble!");
+      return;
+    }
+
+  g_print( " cookie=%d\n", ev->cookie);
+  if (ev->name == NULL)
+    g_string_free (name, TRUE);
+}
+
+void changed_callback (GInotify *inf, gpointer data)
+{
+  GInotifyEvent *ev = (GInotifyEvent*) data;
+  GString* name;
+
+  if (ev->name == NULL)
+    {
+      const gchar* name_chr = "The watch";
+      name = g_string_new (name_chr);
+    }
+  else
+    name = ev->name;
+
+  const char* type = (ev->isdir == TRUE) ? "dir" : "file";
+
+  switch (ev->type)
+    {
+    case G_IN_ATTRIB:
+      g_print ("wd=%d: %s's (%s) metadata changed,", ev->wd, name->str, type);
+      break;
+
+    case G_IN_MODIFY:
+      g_print ("wd=%d: %s (%s) was written,", ev->wd, name->str, type);
+      break;
+
+    case G_IN_MOVE_SELF:
+      g_print ("wd=%d: %s (%s) was moved,", ev->wd, name->str, type);
+      break;
+
+    case G_IN_DELETE_SELF:
+      g_print ("wd=%d: The watch was deleted,", ev->wd);
+      break;
+
+    default:
+      g_print ("This means trouble!\n");
+      return;
+    }
+
+  g_print( " cookie=%d\n", ev->cookie);
+  if (ev->name == NULL)
+    g_string_free (name, TRUE);
+}
+
+void nochange_callback (GInotify *inf, gpointer data)
+{
+  GInotifyEvent *ev = (GInotifyEvent*) data;
+  GString* name;
+
+  if (ev->name == NULL)
+    {
+      const gchar* name_chr = "The watch";
+      name = g_string_new (name_chr);
+    }
+  else
+    name = ev->name;
+
+  const char* type = (ev->isdir == TRUE) ? "dir" : "file";
+
+  switch (ev->type)
+    {
+    case G_IN_CLOSE_WRITE:
+      g_print ("wd=%d: %s (%s) was closed (was writable),", ev->wd, name->str, type);
+      break;
+
+    case G_IN_CLOSE_NOWRITE:
+      g_print ("wd=%d: %s (%s) was closed (was not writable),", ev->wd, name->str, type);
+      break;
+
+    case G_IN_OPEN:
+      g_print ("wd=%d: %s (%s) was opened,", ev->wd, name->str, type);
+      break;
+
+    case G_IN_ACCESS:
+      g_print ("wd=%d: %s (%s) was read,", ev->wd, name->str, type);
+      break;
+
+    default:
+      g_print ("This means trouble!\n");
+      return;
+    }
+
+  g_print( " cookie=%d\n", ev->cookie);
+  if (ev->name == NULL)
+    g_string_free (name, TRUE);
+}
+
+void inf_gen_callback (GInotify *inf, gpointer data)
+{
+  GInotifyEvent *ev = (GInotifyEvent*) data;
+  GString* name;
+
+  if (ev->name == NULL)
+    {
+      const gchar* name_chr = "The watch";
+      name = g_string_new (name_chr);
+    }
+  else
+    name = ev->name;
+
+  switch (ev->type)
+    {
+    case G_IN_UNMOUNT:
+      g_print ("wd=%d: %s was unmounted.\n", ev->wd, name->str);
+      break;
+
+    case G_IN_Q_OVERFLOW:
+      g_print ("The queue overflowed!\n");
+      break;
+
+    case G_IN_IGNORED:
+      g_print ("wd=%d: %s is no longer watched.\n", ev->wd, name->str);
+      /** here we can check if the file stll exists, if yes restart the watch.
+       *  this happens when we edit a file with vi and when we mv another file
+       *  to the file we are watching
+       */
+      break;
+
+    default:
+      g_print ("This means trouble!\n");
+      return;
+    }
+
+  if (ev->name == NULL)
+    g_string_free (name, TRUE);
+}
+
+
+
+int
+main (int argc, char *argv[])
+{
+  GMainLoop *mainloop;
+  GInotify *inf;
+  int i;
+
+  if (argc < 2) {
+    g_warning ("Usage: %s [directories or files ...]\n", argv[0]);
+    return -1;
+  }
+
+
+  inf = g_inotify_new ();
+  if (!inf)
+    return -1;
+
+  g_signal_connect (inf, "directory-specific", G_CALLBACK (dir_callback), inf);
+  g_signal_connect (inf, "attrib-modify-move-delete", G_CALLBACK (changed_callback), inf);
+  g_signal_connect (inf, "open-close-read", G_CALLBACK (nochange_callback), inf);
+  g_signal_connect (inf, "inotify-general", G_CALLBACK (inf_gen_callback), inf);
+
+
+  for (i = 1; i < argc; i++) {
+    g_print ("Adding watch on %s\n", argv[i]);
+    g_inotify_add_watch (inf, argv[i], G_IN_ALL_EVENTS);
+  }
+
+  mainloop = g_main_new (FALSE);
+  g_main_run (mainloop);
+
+  g_object_unref(inf);
+  return 0;
+}
